4cde692276c1730a5e926af858e571d5c2de8e45
Ben Baldwin <1425357+guga31bb@users.noreply.github.com>
2020-03-29 18:48:34 -0400

    Update for NYT 3/28

--- make_figures.R
+++ make_figures.R
@@ -1,11 +1,12 @@
+#if you want to just use the version on github
+#source("https://raw.githubusercontent.com/guga31bb/covid19/master/clean_data.R")
+
 source("clean_data.R")
 
-#get_data is called from clean_data.R
 #argument is how many days to keep after 10th death
 all <- get_data(40) 
 
-#check which states/countries/counties have the highest death count
-#keep the top X to show in plot. and also south korea for the country one
+#keep the top X states/counties/countries with most deaths to show in plot. and also south korea for the country one
 u <- all %>% filter(type == 'us') %>%
   keep_top(10) %>%
   make_figure('nyt')
@@ -16,9 +17,9 @@ c <- all %>% filter(type == 'county') %>%
   keep_top(6) %>%
   make_figure('nyt')
 
-#last nyt update
+#day of last nyt update
 all %>% filter(type =='us') %>% select(date) %>% arrange(date) %>% tail(1)
-#last jhu update
+#day of last jhu update
 all %>% filter(type =='world') %>% select(date) %>% arrange(date) %>% tail(1)
 
 u
